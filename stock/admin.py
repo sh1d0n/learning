3abba5d65acd7fcf66224eb55532f59f2c3543ff
sh1d0n <94841134+sh1d0n@users.noreply.github.com>
2022-10-12 12:06:42 +0300

    Add files via upload

---
+++ stock/admin.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+
+from stock.models import (Stock, Currency, Account, AccountCurrency, AccountStock)
+
+
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ("ticker", "name", "description")
+
+
+@admin.register(Currency)
+class CurrencyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(AccountCurrency)
+class AccountCurrencyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(AccountStock)
+class AccountStockAdmin(admin.ModelAdmin):
+    pass
+
+
+class AccountCurrencyInline(admin.TabularInline):
+    model = AccountCurrency
+
+
+class AccountStockInline(admin.TabularInline):
+    model = AccountStock
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    inlines = [AccountCurrencyInline, AccountStockInline]
